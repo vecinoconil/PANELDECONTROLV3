6b9bd9095a95683fcfbb68d4d446cd0922a87c0e
Bartolome Moreno <vecinoconil75@gmail.com>
2026-04-17 19:19:27 +0200

    chore: wipe repository contents

--- backend/_check_db.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-c = psycopg2.connect(host='core.solba.com', port=5026, dbname='postgres', user='SOLBA', password='solba2012', connect_timeout=10)
-cur = c.cursor()
-cur.execute("SELECT datname, pg_encoding_to_char(encoding), datcollate FROM pg_database WHERE datname ILIKE '%panel%' OR datname ILIKE '%control%'")
-for row in cur.fetchall():
-    print(f"DB: {row[0]}, encoding: {row[1]}, collate: {row[2]}")
-c.close()
